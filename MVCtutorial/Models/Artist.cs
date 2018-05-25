92af388093551c16dcfc9197d71d0b220d2f7866
Robbie McArthur <2098323m@student.gla.ac.uk>
2018-05-25 15:11:00 +0100

    Initial commit

---
+++ MVCtutorial/Models/Artist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCtutorial.Models
+{
+    public class Artist
+    {
+        public int ArtistID { get; set; }
+
+        public String Name { get; set; }
+
+        public List<Album> Albums { get; set; }
+    }
+
+    public class Album
+    {
+        public int AlbumID { get; set; }
+
+        public String Title { get; set; }
+
+        public Artist Artist { get; set; }
+    }
+}
